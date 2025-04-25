8f12f53d76204374924794c0948870020bb65226
Truc292005 <maitruc292005@gmail.com>
2025-04-26 00:55:38 +0700

    ThongKeHoaDon

---
+++ src/cua_hang_tien_loi/ui/admin/ThongKeHoaDon.java
@@ -0,0 +1,358 @@
+package cua_hang_tien_loi.ui.admin;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import cua_hang_tien_loi.utils.StyleUtils;
+import cua_hang_tien_loi.utils.SystemUtils;
+
+public class ThongKeHoaDonTheoNgay extends JFrame implements ActionListener{
+	private JMenuItem itemTaiKhoan;
+	private JMenuItem itemTroGiup;
+	private JMenuItem itemDangXuat;
+	private JMenuItem itemThemSP;
+	private JMenuItem itemTraCuuSP;
+	private JMenuItem itemCapNhatSp;
+	private JMenuItem itemTraCuuKH;
+	private JMenuItem itemThemKH;
+	private JMenuItem itemCapNhatKH;
+	private JMenuItem itemTraCuuHD;
+	private JMenuItem itemThemHD;
+	private JMenuItem itemCapNhatHD;
+	private JMenuItem itemTraCuuNV;
+	private JMenuItem itemThemNV;
+	private JMenuItem itemCapNhatNV;
+	private JMenuItem itemDTTheoNgay;
+	private JMenuItem itemDTTheoThang;
+	private JMenuItem itemDTTheoNam;
+	private JMenuItem itemQuayLai;
+	private JTextField txtMaHD;
+	private JTextField txtNgayLapHD;
+	private JTextField txtSoLuong;
+	private JTextField txtMaNV;
+	private JTextField txtSP;
+	private JTextField txtDonGia;
+	private JButton btnThem;
+	private JButton btnLamMoi;
+	private JButton btnXuatHoaDon;
+	private DefaultTableModel modelTable;
+	private JTable table;
+	private JComboBox cboNgay;
+	private JComboBox cboThang;
+	private JComboBox cboNam;
+	private JButton btnTim;
+	
+	public ThongKeHoaDonTheoNgay () {
+		this.initUIThemHoaDon();
+	}
+	
+	// giao dien
+	
+	private void initUIThemHoaDon() {
+		setTitle("Quản lý cửa hàng tiện lợi - Trang chủ");
+		setSize(1000, 600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		// main
+		JPanel pnMain = new JPanel();
+		pnMain.setLayout(new BorderLayout());
+		
+		// north
+		JPanel pnNorth = new JPanel();
+		pnNorth.setLayout(new FlowLayout(FlowLayout.LEFT));
+//			pnNorth.setBackground(Color.decode("#FAFAFA"));
+		pnNorth.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
+		pnNorth.setPreferredSize(new Dimension(750, 40));
+		
+		// Menu
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBorder(null);
+		
+		// he thong
+		JMenu menuHeThong = new JMenu("Hệ thống");
+		menuHeThong.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/hethong.png"));
+		menuHeThong.setBorderPainted(false);
+		itemTaiKhoan = StyleUtils.createItemMenu("Tài khoản", "src/cua_hang_tien_loi/icon/account.png");
+		itemTroGiup = StyleUtils.createItemMenu("Trợ giúp", "src/cua_hang_tien_loi/icon/helpdesk.png");
+		itemDangXuat = StyleUtils.createItemMenu("Đăng xuất", "src/cua_hang_tien_loi/icon/logout.png");
+		
+		menuHeThong.add(itemTaiKhoan);
+		menuHeThong.addSeparator();
+		menuHeThong.add(itemTroGiup);
+		menuHeThong.addSeparator();
+		menuHeThong.add(itemDangXuat);
+		menuBar.add(menuHeThong);
+		menuBar.add(Box.createHorizontalStrut(25));
+		
+		// san pham
+		JMenu menuSanPham = new JMenu("Sản phẩm");
+		menuSanPham.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/product.png"));
+		itemTraCuuSP = StyleUtils.createItemMenu("Tra cứu", "src/cua_hang_tien_loi/icon/search.png");
+		itemThemSP = StyleUtils.createItemMenu("Thêm", "src/cua_hang_tien_loi/icon/add.png");
+		itemCapNhatSp = StyleUtils.createItemMenu("Cập nhật", "src/cua_hang_tien_loi/icon/edit.png");
+		
+		menuSanPham.add(itemTraCuuSP);
+		menuSanPham.addSeparator();
+		menuSanPham.add(itemThemSP);
+		menuSanPham.addSeparator();
+		menuSanPham.add(itemCapNhatSp);
+		menuBar.add(menuSanPham);
+		menuBar.add(Box.createHorizontalStrut(25));
+		
+		// khach hang
+		JMenu menuKhachHang = new JMenu("Khách hàng");
+		menuKhachHang.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/customer.png"));
+		itemTraCuuKH = StyleUtils.createItemMenu("Tra cứu", "src/cua_hang_tien_loi/icon/search.png");
+		itemCapNhatKH = StyleUtils.createItemMenu("Cập nhật", "src/cua_hang_tien_loi/icon/edit.png");
+		
+		menuKhachHang.add(itemTraCuuKH);
+		menuKhachHang.addSeparator();
+		menuKhachHang.add(itemCapNhatKH);
+		menuBar.add(menuKhachHang);
+		menuBar.add(Box.createHorizontalStrut(25));
+		
+		// hoa don
+		JMenu menuHoaDon = new JMenu("Hoá đơn");
+		menuHoaDon.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/invoice.png"));
+		itemTraCuuHD = StyleUtils.createItemMenu("Tra cứu", "src/cua_hang_tien_loi/icon/search.png");
+		itemThemHD = StyleUtils.createItemMenu("Thêm", "src/cua_hang_tien_loi/icon/add.png");
+		itemCapNhatHD = StyleUtils.createItemMenu("Cập nhật", "src/cua_hang_tien_loi/icon/edit.png");
+		
+		menuHoaDon.add(itemTraCuuHD);
+		menuHoaDon.addSeparator();
+		menuHoaDon.add(itemThemHD);
+		menuHoaDon.addSeparator();
+		menuHoaDon.add(itemCapNhatHD);
+		menuBar.add(menuHoaDon);
+		menuBar.add(Box.createHorizontalStrut(25));
+		
+		// nhan vien
+		JMenu menuNhanVien = new JMenu("Nhân viên");
+		menuNhanVien.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/employee.png"));
+		itemTraCuuNV = StyleUtils.createItemMenu("Tra cứu", "src/cua_hang_tien_loi/icon/search.png");
+		itemThemNV = StyleUtils.createItemMenu("Thêm", "src/cua_hang_tien_loi/icon/add.png");
+		itemCapNhatNV = StyleUtils.createItemMenu("Cập nhật", "src/cua_hang_tien_loi/icon/edit.png");
+		
+		menuNhanVien.add(itemTraCuuNV);
+		menuNhanVien.addSeparator();
+		menuNhanVien.add(itemThemNV);
+		menuNhanVien.addSeparator();
+		menuNhanVien.add(itemCapNhatNV);
+		menuBar.add(menuNhanVien);
+		menuBar.add(Box.createHorizontalStrut(25));
+		
+		// thong ke
+		JMenu menuThongKe = new JMenu("Thống kê");
+		menuThongKe.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/thongke.png"));
+		
+		JMenu itemDoanhThu = new JMenu("Doanh thu");
+		itemDoanhThu.setIcon(new ImageIcon("src/cua_hang_tien_loi/icon/doanhthu.png"));
+		
+		itemDTTheoNgay = StyleUtils.createItemMenu("Theo ngày", "src/cua_hang_tien_loi/icon/day.png");
+		itemDTTheoThang = StyleUtils.createItemMenu("Theo tháng", "src/cua_hang_tien_loi/icon/month.png");
+		itemDTTheoNam = StyleUtils.createItemMenu("Theo năm", "src/cua_hang_tien_loi/icon/year.png");
+		
+		itemDoanhThu.add(itemDTTheoNgay);
+		itemDoanhThu.addSeparator();
+		itemDoanhThu.add(itemDTTheoThang);
+		itemDoanhThu.addSeparator();
+		itemDoanhThu.add(itemDTTheoNam);
+		
+		menuThongKe.add(itemDoanhThu);
+		
+		menuBar.add(menuThongKe);
+		menuBar.add(Box.createHorizontalStrut(25));
+		
+		// quay lai
+		menuBar.add(Box.createVerticalStrut(10));
+		itemQuayLai = StyleUtils.createItemMenu("Quay lại (F1)", "src/cua_hang_tien_loi/icon/quaylai.png");
+		menuBar.add(itemQuayLai);
+		
+		pnNorth.add(menuBar);
+		pnNorth.add(Box.createVerticalStrut(5));
+		
+		pnMain.add(pnNorth, BorderLayout.NORTH);
+		
+		// cen
+		JPanel pnCen = new JPanel();
+		
+		JPanel pn = new JPanel();
+		pn.setLayout(new BoxLayout(pn, BoxLayout.Y_AXIS));
+		JLabel lbTitle = StyleUtils.createHeaderTitle("THỐNG KÊ HÓA ĐƠN THEO NGÀY");
+		lbTitle.setAlignmentX(CENTER_ALIGNMENT);
+		lbTitle.setFont(new Font("Arial", Font.BOLD, 20));
+		lbTitle.setForeground(Color.BLACK);
+		pn.add(lbTitle);
+		
+		JPanel pn1 = new JPanel();
+		pn1.setLayout(new BoxLayout(pn1, BoxLayout.X_AXIS));
+		JLabel lblNgay = new JLabel("Thời gian thống kê:");
+		cboNgay = new JComboBox<>();
+		cboNgay.addItem("1");
+		cboNgay.setPreferredSize(new Dimension(220, 25));
+		JLabel lblThang = new JLabel("Tháng:");
+		cboThang = new JComboBox<>();
+		cboThang.addItem("1");
+		cboThang.setPreferredSize(new Dimension(220, 25));
+		JLabel lblNam = new JLabel("Năm:");
+		cboNam = new JComboBox<>();
+		cboNam.addItem("2000");
+		cboNam.setPreferredSize(new Dimension(220, 25));
+		pn1.add(lblNgay);
+		pn1.add(Box.createHorizontalStrut(10));
+		pn1.add(cboNgay);
+		pn1.add(Box.createHorizontalStrut(10));
+		pn1.add(lblThang);
+		pn1.add(Box.createHorizontalStrut(10));
+		pn1.add(cboThang);
+		pn1.add(Box.createHorizontalStrut(10));
+		pn1.add(lblNam);
+		pn1.add(Box.createHorizontalStrut(10));
+		pn1.add(cboNam);
+		pn.add(Box.createVerticalStrut(20));
+		pn.add(pn1);
+		
+		
+		//button
+		JPanel pn2 = new JPanel();
+		pn2.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		btnTim = new JButton("Tìm kiếm", new ImageIcon("src/cua_hang_tien_loi/icon/search.png"));
+		btnLamMoi = new JButton("Làm mới", new ImageIcon("src/cua_hang_tien_loi/icon/reload.png"));
+		btnXuatHoaDon = new JButton("Xuất hóa đơn", new ImageIcon("src/cua_hang_tien_loi/icon/xuathd.png"));
+		pn2.add(btnTim);
+		pn.add(Box.createVerticalStrut(10));
+		pn.add(pn2);
+		
+		
+		pnCen.add(pn);
+		
+		
+		JPanel pnSouth = new JPanel();
+
+		String[] title = { "Mã HD", "Ngày lập HD", "Số lượng", "Mã NV", "Mã SP", "Đơn giá" };
+		modelTable = new DefaultTableModel(title, 0);
+		table = new JTable(modelTable);
+		JScrollPane scroll = new JScrollPane(table);
+
+		scroll.setPreferredSize(new Dimension(1000, 340));
+		pnSouth.add(new JScrollPane(scroll));
+
+		pnMain.add(pnSouth, BorderLayout.SOUTH);
+		pnMain.add(pnCen, BorderLayout.CENTER);
+		
+		add(pnMain);
+		
+		
+		
+		
+		
+		// event
+		
+		// he thong
+		itemTaiKhoan.addActionListener(this);
+		itemTroGiup.addActionListener(this);
+		itemDangXuat.addActionListener(this);
+		
+		// san pham
+		itemTraCuuSP.addActionListener(this);
+		itemCapNhatSp.addActionListener(this);
+		itemThemSP.addActionListener(this);
+		
+		// khach hang
+		itemTraCuuKH.addActionListener(this);
+		itemCapNhatKH.addActionListener(this);
+		
+		// hoa don
+		itemTraCuuHD.addActionListener(this);
+		itemThemHD.addActionListener(this);
+		
+		// nhan vien
+		itemTraCuuNV.addActionListener(this);
+		itemCapNhatNV.addActionListener(this);
+		itemThemNV.addActionListener(this);
+		
+		// thong ke
+		itemDTTheoNgay.addActionListener(this);
+		itemDTTheoThang.addActionListener(this);
+		itemDTTheoNam.addActionListener(this);
+		
+		// quay lai
+		itemQuayLai.addActionListener(this);
+		
+		// key f1
+		SystemUtils.setF1ToKey(pnMain, "F1", itemQuayLai);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object source = e.getSource();
+
+		if (source.equals(itemTaiKhoan)) {
+			new ThongTinTaiKhoanQuanLy().setVisible(true);
+		} else if (source.equals(itemTroGiup)) {
+			SystemUtils.openFile("/Users/lethoa/Documents/giaykhamsuckhoe.pdf");
+		} else if (source.equals(itemDangXuat)) {
+			SystemUtils.dangXuat(this);
+		} else if (source.equals(itemThemSP)) {
+			this.setVisible(false);
+			new ThemSanPhamQuanLy().setVisible(true);
+		} else if (source.equals(itemCapNhatSp)) {
+			this.setVisible(false);
+			new CapNhatSanPhamQuanLy().setVisible(true);
+		} else if (source.equals(itemTraCuuKH)) {
+			this.setVisible(false);
+			new TraCuuKhachHangQuanLy().setVisible(true);
+//		} else if (source.equals(itemCapNhatKH)) {
+//			this.setVisible(false);
+//			new CapNhatThongTinKhachHangQuanLy().setVisible(true);
+		} else if (source.equals(itemTraCuuHD)) {
+			this.setVisible(false);
+
+		} else if (source.equals(itemThemHD)) {
+			this.setVisible(false);
+
+		} else if (source.equals(itemCapNhatHD)) {
+			this.setVisible(false);
+
+		} else if (source.equals(itemTraCuuNV)) {
+			this.setVisible(false);
+			new TraCuuNhanVien().setVisible(true);
+		} else if (source.equals(itemThemNV)) {
+			this.setVisible(false);
+			new ThemNhanVien().setVisible(true);
+		} else if (source.equals(itemCapNhatNV)) {
+			this.setVisible(false);
+			new CapNhatNhanVien().setVisible(true);
+		} else if (source.equals(itemQuayLai)) {
+			SystemUtils.quayLai(this);
+		}
+	}
+	
+	public static void main(String[] args) {
+		new ThongKeHoaDonTheoNgay().setVisible(true);
+	}
+}
